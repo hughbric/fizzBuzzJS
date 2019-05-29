1bf29e33639ec17f498dd08ee60f2eeece98c79c
Michael Hughes <mhughes36@live.co.uk>
2019-05-29 16:56:06 +0100

    FizzBuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,9 +2,13 @@ describe('FizzBuzz', function() {
 
   it('should return Fizz when 3', function() {
     expect(fizzBuzz(3)).toEqual('Fizz');
-  });
+    });
 
   it('should return Buzz when 5', function() {
     expect(fizzBuzz(5)).toEqual('Buzz');
-  });
+    });
+
+  it('should return FizzBuzz when 15', function() {
+    expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,9 @@
 function fizzBuzz(number) {
-  if (number === 3) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (number % 3 === 0) {
     return 'Fizz';
-  }
-  if (number === 5) {
+  } else if (number % 5 === 0) {
     return 'Buzz';
   }
 }
